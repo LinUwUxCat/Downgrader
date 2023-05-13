0fd1dd9fdc9559977b28cbe6af2fc464e05d4ed8
LinuxCat <masch77.linuxcat@gmail.com>
2023-05-13 20:31:00 +0200

    Mediatracker works - Added list of supported mediatracker blocks - Added downgrading a clip - Added downgrading a clip group - Moved block list downgrading logic to its own function

--- Downgrader/blocklist.cs
+++ Downgrader/blocklist.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using GBX.NET.Engines.Game;
 namespace Blocklist{
     class TMNF{
         public static ReadOnlyCollection<string> Blocks = new List<string>{
@@ -303,6 +304,25 @@ namespace Blocklist{
             "StadiumWaterClip"
 
         }.AsReadOnly();
+        public static HashSet<Type> MediaBlocks = new HashSet<Type>{
+            typeof(CGameCtnMediaBlockTriangles2D),
+            typeof(CGameCtnMediaBlockCameraEffectShake),
+            typeof(CGameCtnMediaBlockTransitionFade),
+            typeof(CGameCtnMediaBlockTrails),
+            typeof(CGameCtnMediaBlockCameraPath),
+            typeof(CGameCtnMediaBlockCameraCustom),
+            typeof(CGameCtnMediaBlockCameraGame), //Camera race
+            typeof(CGameCtnMediaBlockFxBlurMotion),
+            typeof(CGameCtnMediaBlockFxBlurDepth),
+            typeof(CGameCtnMediaBlockFxBloom),
+            typeof(CGameCtnMediaBlockFxColors),
+            typeof(CGameCtnMediaBlock3dStereo),
+            typeof(CGameCtnMediaBlockMusicEffect), //Music volume
+            typeof(CGameCtnMediaBlockText),
+            typeof(CGameCtnMediaBlockImage),
+            typeof(CGameCtnMediaBlockSound),
+            typeof(CGameCtnMediaBlockGhost)
+        };
     }
 
     class TMNESWC{
@@ -433,5 +453,18 @@ namespace Blocklist{
             "StadiumTubeRoad",
             "StadiumTubeRoadUp"
         }.AsReadOnly();
+         public static HashSet<Type> MediaBlocks = new HashSet<Type>{
+            typeof(CGameCtnMediaBlockCameraEffectShake),
+            typeof(CGameCtnMediaBlockTransitionFade),
+            typeof(CGameCtnMediaBlockCameraPath), //Camera -> camera path
+            typeof(CGameCtnMediaBlockCameraCustom), //Camera -> camera custom
+            typeof(CGameCtnMediaBlockCameraGame), //Camera -> Camera race
+            typeof(CGameCtnMediaBlockFxBlurDepth), //FX blur
+            typeof(CGameCtnMediaBlockFxColors),
+            typeof(CGameCtnMediaBlockMusicEffect), //Music volume
+            typeof(CGameCtnMediaBlockText),
+            typeof(CGameCtnMediaBlockImage),
+            typeof(CGameCtnMediaBlockSound)
+        };
     }
 }
